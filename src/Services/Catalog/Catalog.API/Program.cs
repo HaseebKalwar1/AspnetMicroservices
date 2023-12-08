9fbee512834707cdbfbaa514bcaf4ee82f2d3ff8
HaseebKalwar1 <haseebkalwar.hgc1@gmail.com>
2023-12-08 16:32:58 -0500

    Catalog API Finalzied

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,11 +1,18 @@
+using Catalog.API.Data.Interfaces;
+using Catalog.API.Data;
+using Catalog.API.Repositories.Interfaces;
+using Catalog.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
